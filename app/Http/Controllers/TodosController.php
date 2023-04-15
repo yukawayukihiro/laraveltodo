9141cce1c201ab36ee9df19c5a5e045fbc3723cb
yukawa <metalg9123@i.softbank.jp>
2023-04-15 20:24:27 +0900

    トップ画面作成

--- app/Http/Controllers/TodosController.php
+++ app/Http/Controllers/TodosController.php
@@ -3,8 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Todos;
 
 class TodosController extends Controller
 {
-    //
+    //todoリスト画面を表示
+    public function index () {
+        $todos = Todos::orderBy('id','asc')->get();
+        return view('todo_list',compact('todos',$todos));
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,4 @@
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/','TodosController@index')->name('index');
